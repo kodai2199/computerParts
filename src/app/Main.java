ef7c86a8c5979eac001f839e266bae5a8d6b9192
kodai2199 <ganzerli.giulio.99@gmail.com>
2020-05-12 17:07:48 +0200

    Added an insanely small part of the GUI

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -15,6 +16,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
