22c516822d36601770a06f7847bf4a5773dbdd42
kodai2199 <ganzerli.giulio.99@gmail.com>
2020-05-13 17:22:04 +0200

    Added test

--- src/app/GuiController.java
+++ src/app/GuiController.java
@@ -1,5 +1,26 @@
 package app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class GuiController {
 	
+	@FXML
+	private TextField user;
+	
+	@FXML
+	private PasswordField pwd;
+	
+	public void initialize() {
+        // initialization here, if needed...
+    }
+	
+	@FXML
+	public void login() {
+		String user = this.user.getText();
+		String pwd = this.pwd.getText();
+		System.out.println("Fa schifo, lo so, ma "+user+" ha provato a fare il login con la password "+pwd);
+	}
 }
